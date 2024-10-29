0cde43986bee802e411a444a5d61f999ada8e812
sifter0926 <sifter0926@gmail.com>
2024-10-29 10:06:38 +0900

    커밋1

--- src/main/java/org/pgm/securitydemo/config/CustomSecurityConfig.java
+++ src/main/java/org/pgm/securitydemo/config/CustomSecurityConfig.java
@@ -1,4 +1,56 @@
 package org.pgm.securitydemo.config;
 
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class CustomSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequestsConfigurer -> authorizeHttpRequestsConfigurer
+                .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                .requestMatchers("/login", "/sinup", "/user/**", "/","/all").permitAll()
+                .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated())
+                .formLogin(formLoginConfigurer -> formLoginConfigurer
+                        .loginPage("/user/login")
+                        .loginProcessingUrl("/loginProcess")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true))
+                .build();
+    }
+
+
+    @Bean // password 암호화 빈
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring() // 모든 스태틱 자원 필터 제외
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        //.requestMatchers("/static/**");
+    }
+
+
 }
